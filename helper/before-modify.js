adf81fce082ea7196dae4d5889df1a1024401550
LiangPeng <liangpeng.net@gmail.com>
2017-03-20 23:04:21 +0800

    根据beforeAdd，在beforeModify中也增加了unique验证

--- helper/before-modify.js
+++ helper/before-modify.js
@@ -8,29 +8,56 @@ var delegate      = require('func-delegate')
  * hook 必选, 实例的存放位置
  * cols 可选, 允许修改的字段
  */
-var beforeModify = function(Model, hook, cols) {
-
-  return function(req, res, next) {
-    var model = req.hooks[hook]
-      , attr;
-    cols = cols || Model.editableCols || Model.writableCols;
-    try {
-      attr = U.pickParams(req, cols, Model);
-    } catch (e) {
-      return next(e);
-    }
-    delete attr.id
-    _.each(attr, function(v, k) {
-      if (model[k] === v) return;
-      model[k] = v;
-    });
-    next();
-  };
-
-};
-
 module.exports = function(rest) {
   var Sequelize = rest.Sequelize;
+  var beforeModify = function(Model, hook, cols) {
+
+    return function(req, res, next) {
+      var model = req.hooks[hook]
+        , attr;
+      cols = cols || Model.editableCols || Model.writableCols;
+      try {
+        attr = U.pickParams(req, cols, Model);
+      } catch (e) {
+        return next(e);
+      }
+
+      var _save = function() {
+        delete attr.id
+        _.each(attr, function(v, k) {
+          if (model[k] === v) return;
+          model[k] = v;
+        });
+        next();
+      }
+
+      if (!!Model.unique){
+        // 如果设置了唯一属性
+        // editableCols可能设置的没有id
+        var where = { 
+          id: { $ne : attr.id?attr.id : model.id } 
+        };
+        _.each(Model.unique, function(x) {
+          where[x] = attr[x];
+        });
+        if(Model.rawAttributes.isDelete){
+          where['isDelete'] = 'no';
+        }
+
+        // 根据条件查找资源
+        Model.findOne({where: where}).then(function(model) {
+          if (model) {
+            // 资源已经存在，重复了
+            return next(rest.errors.ifError(Error('Resource exists.'), Model.unique[0]))
+          }
+          // 保存资源
+          return _save(model);
+        }).catch(next);
+      }else{
+        return _save();
+      }
+    };
+  };
 
   var schemas = [{
     name: 'Model',
