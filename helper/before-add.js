85acb5103fe6ebfd17af44e6037a8f4eb5b018b1
Redstone Zhao <13740080@qq.com>
2017-06-01 21:51:01 +0000

    helper.before-add es5 => es6.airbnb

--- helper/before-add.js
+++ helper/before-add.js
@@ -1,9 +1,9 @@
-var delegate      = require('func-delegate')
-  , U             = require('../lib/utils')
-  , _             = require('lodash');
+const delegate = require('func-delegate');
+const U = require('../lib/utils');
+const _ = require('lodash');
 
-module.exports = function(rest) {
-  var Sequelize = rest.Sequelize;
+module.exports = (rest) => {
+  const Sequelize = rest.Sequelize;
 
   /**
    * 修改某个资源描述的前置方法, 不会sync到数据库
@@ -11,19 +11,16 @@ module.exports = function(rest) {
    * cols 可选, 允许设置的字段
    * hook 必选, 生成实例的存放位置
    */
-  var beforeAdd = function(Model, cols, hook) {
-
-    return function(req, res, next) {
-      var attr = U.pickParams(req, cols || Model.writableCols, Model);
+  const beforeAdd = (Model, cols, hook) => (
+    (req, res, next) => {
+      const attr = U.pickParams(req, cols || Model.writableCols, Model);
 
       // 存储数据
-      var _save = function(model) {
-        model.save().then(function(mod) {
+      const _save = (model) => {
+        model.save().then((mod) => {
           req.hooks[hook] = mod;
           next();
-        }).catch(function(error) {
-          return next(rest.errors.sequelizeIfError(error))
-        });
+        }).catch((error) => next(rest.errors.sequelizeIfError(error)));
       };
 
       // 约定的 creatorId, 等于 req.user.id
@@ -38,13 +35,13 @@ module.exports = function(rest) {
 
       // 如果设置了唯一属性，且开启了回收站功能
       // 则判断是否需要执行恢复操作
-      var where = {};
-      _.each(Model.unique, function(x) {
+      const where = {};
+      _.each(Model.unique, (x) => {
         where[x] = attr[x];
       });
 
       // 根据条件查找资源
-      Model.findOne({where: where}).then(function(model) {
+      return Model.findOne({ where }).then((model) => {
         // 资源存在
         if (model) {
           // 且资源曾经被删除
@@ -54,47 +51,43 @@ module.exports = function(rest) {
             model.isDelete = 'no';
           } else {
             // 资源已经存在，重复了
-            return next(rest.errors.ifError(Error('Resource exists.'), Model.unique[0]))
+            return next(rest.errors.ifError(Error('Resource exists.'), Model.unique[0]));
           }
-        } else {
-          // 构建一个全新的资源
-          model = Model.build(attr);
         }
-        // 保存资源
-        _save(model);
+        // 保存资源 已有资源或者构建一个全新的资源
+        return _save(model || Model.build(attr));
       }).catch(next);
-    };
+    }
+  );
 
-  };
-
-  var schemas = [{
+  const schemas = [{
     name: 'Model',
     type: Sequelize.Model,
-    message: 'Model must be a class of Sequelize defined'
+    message: 'Model must be a class of Sequelize defined',
   }, {
     name: 'cols',
     type: Array,
     allowNull: true,
     validate: {
-      check: function(keys, schema, args) {
-        var Model = args[0];
-        _.each(keys, function(v) {
+      check(keys, schema, args) {
+        const Model = args[0];
+        _.each(keys, (v) => {
           if (!_.isString(v)) {
             throw Error('Every item in cols must be a string.');
           }
           if (!Model.rawAttributes[v]) {
-            throw Error('Attr non-exists: ' + v);
+            throw Error(`Attr non-exists: ${v}`);
           }
         });
         return true;
-      }
+      },
     },
-    message: "Allow writed attrs's name array"
+    message: 'Allow writed attrs\'s name array',
   }, {
     name: 'hook',
     type: String,
     allowNull: false,
-    message: 'Added instance will hook on req.hooks[hook], so `hook` must be a string'
+    message: 'Added instance will hook on req.hooks[hook], so `hook` must be a string',
   }];
 
   return delegate(beforeAdd, schemas);
