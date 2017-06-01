0251bd92dfce8d6623ee248cef821150215d46ba
Redstone Zhao <13740080@qq.com>
2017-06-01 18:51:00 +0000

    全面拥抱 es6，增加 eslint/airbnb 规范

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-module.exports = function(rest) {
-  return rest.helper.rest = {
-    detail: require('./helper/detail')(rest),
-    list: require('./helper/list')(rest),
-    beforeModify: require('./helper/before-modify')(rest),
-    save: require('./helper/save')(rest),
-    modify: require('./helper/modify')(rest),
-    beforeAdd: require('./helper/before-add')(rest),
-    add: require('./helper/add')(rest),
-    batchAdd: require('./helper/batch-add')(rest),
-    remove: require('./helper/remove')(rest),
-    statistics: require('./helper/statistics')(rest)
+const detail = require('./helper/detail');
+const list = require('./helper/list');
+const beforeModify = require('./helper/before-modify');
+const save = require('./helper/save');
+const modify = require('./helper/modify');
+const beforeAdd = require('./helper/before-add');
+const add = require('./helper/add');
+const batchAdd = require('./helper/batch-add');
+const remove = require('./helper/remove');
+const statistics = require('./helper/statistics');
+
+module.exports = (rest) => {
+  rest.helper.rest = {
+    detail: detail(rest),
+    list: list(rest),
+    beforeModify: beforeModify(rest),
+    save: save(rest),
+    modify: modify(rest),
+    beforeAdd: beforeAdd(rest),
+    add: add(rest),
+    batchAdd: batchAdd(rest),
+    remove: remove(rest),
+    statistics: statistics(rest),
   };
+  return rest.helper.rest;
 };
