f1793a27e6ec2e1218af4bea9c5412a940484a6b
Redstone Zhao <13740080@qq.com>
2017-06-01 21:35:06 +0000

    helper.add es5 => es6.airbnb

--- helper/add.js
+++ helper/add.js
@@ -1,11 +1,10 @@
-var delegate      = require('func-delegate')
-  , U             = require('../lib/utils')
-  , beforeAdd     = require('./before-add')
-  , detailHelper  = require('./detail')
-  , _             = require('lodash');
+const delegate = require('func-delegate');
+const beforeAdd = require('./before-add');
+const detailHelper = require('./detail');
+const _ = require('lodash');
 
-module.exports = function(rest) {
-  var Sequelize = rest.Sequelize;
+module.exports = (rest) => {
+  const Sequelize = rest.Sequelize;
 
 
   /**
@@ -15,67 +14,65 @@ module.exports = function(rest) {
    * hook 必选, 实例的存放位置
    * attachs 可选，要附加输出的数据格式为 key => value, value 是 req 上的路径字符串
    */
-  var add = function(Model, cols, hook, attachs) {
-
+  const add = (Model, cols, hook, attachs) => {
     // 这里hook其实是必须的，因为这里把 add 分成两个部分，
     // 为了避免冲突导致，这里引入了随机字符串
-    if (!hook) hook = Model.name + '_' + rest.utils.randStr(10);
+    const _hook = hook || `${Model.name}_${rest.utils.randStr(10)}`;
 
-    var before = beforeAdd(rest)(Model, cols, hook);
-    var detail = detailHelper(rest)(hook, attachs, 201);
+    const before = beforeAdd(rest)(Model, cols, _hook);
+    const detail = detailHelper(rest)(_hook, attachs, 201);
 
-    return function(req, res, next) {
-      before(req, res, function(error) {
+    return (req, res, next) => {
+      before(req, res, (error) => {
         if (error) return next(error);
-        detail(req, res, next);
+        return detail(req, res, next);
       });
     };
-
   };
 
-  var schemas = [{
+  const schemas = [{
     name: 'Model',
     type: Sequelize.Model,
-    message: 'Model must be a class of Sequelize defined'
+    message: 'Model must be a class of Sequelize defined',
   }, {
     name: 'cols',
     type: Array,
     allowNull: true,
     validate: {
-      check: function(keys, schema, args) {
-        var Model = args[0];
-        _.each(keys, function(v) {
+      check(keys, schema, args) {
+        const Model = args[0];
+        _.each(keys, (v) => {
           if (!_.isString(v)) {
             throw Error('Every item in cols must be a string.');
           }
           if (!Model.rawAttributes[v]) {
-            throw Error('Attr non-exists: ' + v);
+            throw Error(`Attr non-exists: ${v}`);
           }
         });
         return true;
-      }
+      },
     },
-    message: "Allow writed attrs's name array"
+    message: 'Allow writed attrs\'s name array',
   }, {
     name: 'hook',
     type: String,
     allowNull: true,
-    message: 'Added instance will hook on req.hooks[hook], so `hook` must be a string'
+    message: 'Added instance will hook on req.hooks[hook], so `hook` must be a string',
   }, {
     name: 'attachs',
     type: Object,
     allowNull: true,
     validate: {
-      check: function(value) {
-        _.each(value, function(v) {
+      check(value) {
+        _.each(value, (v) => {
           if (!_.isString(v)) {
             throw Error('The attachs structure is key = > value, value must be a string');
           }
         });
         return true;
-      }
+      },
     },
-    message: 'Attach other data dict. key => value, value is req\'s path'
+    message: 'Attach other data dict. key => value, value is req\'s path',
   }];
 
   return delegate(add, schemas);
