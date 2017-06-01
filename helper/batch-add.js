1bbed6e959d4f06024ef16cd1f6e5dfcf01f8c08
Redstone Zhao <13740080@qq.com>
2017-06-01 22:01:37 +0000

    helper.batch-add es5 => es6.airbnb

--- helper/batch-add.js
+++ helper/batch-add.js
@@ -1,21 +1,19 @@
-var delegate      = require('func-delegate')
-  , U             = require('../lib/utils')
-  , beforeAdd     = require('./before-add')
-  , detailHelper  = require('./detail')
-  , async         = require('async')
-  , _             = require('lodash');
+const delegate = require('func-delegate');
+const U = require('../lib/utils');
+const async = require('async');
+const _ = require('lodash');
 
-module.exports = function(rest) {
-  var Sequelize = rest.Sequelize;
+module.exports = (rest) => {
+  const Sequelize = rest.Sequelize;
 
   /** 输出 */
-  var detail = function(hook, attachs) {
-    return function(req, res, next) {
-      var results = _.isArray(req.body) ? req.hooks[hook] : [req.hooks[hook]];
-      var ret = _.map(results, function(model) {
-        var json = (model.toJSON instanceof Function) ? model.toJSON() : model;
+  const detail = (hook, attachs) => (
+    (req, res, next) => {
+      const results = _.isArray(req.body) ? req.hooks[hook] : [req.hooks[hook]];
+      let ret = _.map(results, (model) => {
+        const json = (model.toJSON instanceof Function) ? model.toJSON() : model;
         if (attachs) {
-          _.each(attachs, function(v, k) {
+          _.each(attachs, (v, k) => {
             json[k] = _.get(req, v);
           });
         }
@@ -28,47 +26,43 @@ module.exports = function(rest) {
         res.send(201, ret);
       }
       next();
-    };
-  };
+    }
+  );
 
   /** 批量验证 */
-  var validate = function(Model, cols, hook) {
-    return function(req, res, next) {
-      var body = _.isArray(req.body) ? req.body : [req.body];
-      var origParams = _.clone(req.params);
-      var handler = function(params, callback) {
-        var attr;
-
+  const validate = (Model, cols, hook) => (
+    (req, res, next) => {
+      const body = _.isArray(req.body) ? req.body : [req.body];
+      const origParams = _.clone(req.params);
+      const handler = (params, callback) => {
         req.params = _.extend(params, origParams);
-        attr = U.pickParams(req, cols || Model.writableCols, Model)
+        const attr = U.pickParams(req, cols || Model.writableCols, Model);
         if (Model.rawAttributes.creatorId) attr.creatorId = req.user.id;
         if (Model.rawAttributes.clientIp) attr.clientIp = rest.utils.clientIp(req);
 
         /** 构建实例 */
-        model = Model.build(attr);
-        model.validate().then(function() {
+        const model = Model.build(attr);
+        model.validate().then(() => {
           callback(null, model);
         }).catch(callback);
       };
-      async.map(body, handler, function(error, results) {
-        var err = rest.errors.sequelizeIfError(error);
+      async.map(body, handler, (error, results) => {
+        const err = rest.errors.sequelizeIfError(error);
         if (err) return next(err);
         req.hooks[hook] = results;
-        next();
+        return next();
       });
-    };
-  };
+    }
+  );
 
   /** 保存 */
-  var save = function(hook, Model) {
-    return function(req, res, next) {
-      var ls = _.map(req.hooks[hook], function(x) {
-        return (x.toJSON instanceof Function) ? x.toJSON() : x;
-      });
-      var p = _.isArray(req.body) ? Model.bulkCreate(ls) : Model.create(ls[0]);
-      rest.utils.callback(p, function(error, results) {
-        var err = rest.errors.sequelizeIfError(error);
-        if (err) return next(err);
+  const save = (hook, Model) => (
+    (req, res, next) => {
+      const ls = _.map(req.hooks[hook], (x) => ((x.toJSON instanceof Function) ? x.toJSON() : x));
+      const p = _.isArray(req.body) ? Model.bulkCreate(ls) : Model.create(ls[0]);
+      rest.utils.callback(p, (error, results) => {
+        const err1 = rest.errors.sequelizeIfError(error);
+        if (err1) return next(err1);
         req.hooks[hook] = results;
 
         /**
@@ -76,75 +70,75 @@ module.exports = function(rest) {
          * 而如果是单一的是要返回具体的内容，所以要 model.reload
          */
         if (_.isArray(results)) return next();
-        rest.utils.callback(results.reload(), function(error) {
-          var err = rest.errors.sequelizeIfError(error);
-          if (err) return next(err);
-          next();
+        return rest.utils.callback(results.reload(), (e) => {
+          const err2 = rest.errors.sequelizeIfError(e);
+          if (err2) return next(err2);
+          return next();
         });
       });
-    };
-  };
+    }
+  );
 
   /** 批量添加 */
-  var batchAdd = function(Model, cols, hook, attachs) {
-    var hook = hook || Model.name + 's';
-    var _validate = validate(Model, cols, hook);
-    var _save = save(hook, Model);
-    var _detail = detail(hook, attachs);
-    return function(req, res, next) {
-      _validate(req, res, function(error) {
-        if (error) return next(error);
-        _save(req, res, function(error) {
-          if (error) return next(error);
-          _detail(req, res, next);
+  const batchAdd = (Model, cols, hook, attachs) => {
+    const _hook = hook || `${Model.name}s`;
+    const _validate = validate(Model, cols, _hook);
+    const _save = save(_hook, Model);
+    const _detail = detail(_hook, attachs);
+    return (req, res, next) => {
+      _validate(req, res, (error1) => {
+        if (error1) return next(error1);
+        return _save(req, res, (error2) => {
+          if (error2) return next(error2);
+          return _detail(req, res, next);
         });
       });
     };
   };
 
-  var schemas = [{
+  const schemas = [{
     name: 'Model',
     type: Sequelize.Model,
-    message: 'Model must be a class of Sequelize defined'
+    message: 'Model must be a class of Sequelize defined',
   }, {
     name: 'cols',
     type: Array,
     allowNull: true,
     validate: {
-      check: function(keys, schema, args) {
-        var Model = args[0];
-        _.each(keys, function(v) {
+      check(keys, schema, args) {
+        const Model = args[0];
+        _.each(keys, (v) => {
           if (!_.isString(v)) {
             throw Error('Every item in cols must be a string.');
           }
           if (!Model.rawAttributes[v]) {
-            throw Error('Attr non-exists: ' + v);
+            throw Error(`Attr non-exists: ${v}`);
           }
         });
         return true;
-      }
+      },
     },
-    message: "Allow writed attrs's name array"
+    message: 'Allow writed attrs\'s name array',
   }, {
     name: 'hook',
     type: String,
     allowNull: true,
-    message: 'Added instance will hook on req.hooks[hook], so `hook` must be a string'
+    message: 'Added instance will hook on req.hooks[hook], so `hook` must be a string',
   }, {
     name: 'attachs',
     type: Object,
     allowNull: true,
     validate: {
-      check: function(value) {
-        _.each(value, function(v) {
+      check(value) {
+        _.each(value, (v) => {
           if (!_.isString(v)) {
             throw Error('The attachs structure is key = > value, value must be a string');
           }
         });
         return true;
-      }
+      },
     },
-    message: 'Attach other data dict. key => value, value is req\'s path'
+    message: 'Attach other data dict. key => value, value is req\'s path',
   }];
 
   return delegate(batchAdd, schemas);
