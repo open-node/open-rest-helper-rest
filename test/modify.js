097303cbd0d3d867b69d95061ffa2bf412d52913
Redstone Zhao <13740080@qq.com>
2017-06-02 00:09:18 +0000

    eslint/airbnb

--- test/modify.js
+++ test/modify.js
@@ -1,269 +1,254 @@
-var assert      = require('assert')
-  , rest        = require('open-rest')
-  , om          = require('open-rest-with-mysql')(rest)
-  , Sequelize   = rest.Sequelize
-  , helper      = require('../')(rest);
+const assert = require('assert');
+const rest = require('open-rest');
+require('open-rest-with-mysql')(rest);
 
-var sequelize = new Sequelize();
-var Model = sequelize.define('book', {
+const Sequelize = rest.Sequelize;
+const helper = require('../')(rest);
+
+const sequelize = new Sequelize();
+const Model = sequelize.define('book', {
   id: {
     type: Sequelize.INTEGER.UNSIGNED,
     primaryKey: true,
-    autoIncrement: true
+    autoIncrement: true,
   },
-  name: Sequelize.STRING(100),
-  age: Sequelize.INTEGER.UNSIGNED
+  name: Sequelize.STRING,
+  age: Sequelize.INTEGER.UNSIGNED,
 });
 
-describe("open-rest-helper-rest-modify", function() {
-
-  describe("Argument validate error", function() {
-
-    it("Model argument unset", function(done) {
-      assert.throws(function() {
+describe('open-rest-helper-rest-modify', () => {
+  describe('Argument validate error', () => {
+    it('Model argument unset', (done) => {
+      assert.throws(() => {
         helper.modify();
-      }, function(err) {
-        return err instanceof Error && err.message === 'Model must be a class of Sequelize defined'
+      }, (err) => {
+        const msg = 'Model must be a class of Sequelize defined';
+        return err instanceof Error && err.message === msg;
       });
       done();
     });
 
-    it("Model argument type error", function(done) {
-      assert.throws(function() {
+    it('Model argument type error', (done) => {
+      assert.throws(() => {
         helper.modify({});
-      }, function(err) {
-        return err instanceof Error && err.message === 'Model must be a class of Sequelize defined'
+      }, (err) => {
+        const msg = 'Model must be a class of Sequelize defined';
+        return err instanceof Error && err.message === msg;
       });
       done();
     });
 
-    it("hook argument type error", function(done) {
-      assert.throws(function() {
+    it('hook argument type error', (done) => {
+      assert.throws(() => {
         helper.modify(Model, {});
-      }, function(err) {
-        return err instanceof Error && err.message === 'Will modify instance hook on req.hooks[hook], so `hook` must be a string'
+      }, (err) => {
+        const msg = 'Will modify instance hook on req.hooks[hook], so `hook` must be a string';
+        return err instanceof Error && err.message === msg;
       });
       done();
     });
 
-    it("cols type error", function(done) {
-      assert.throws(function() {
+    it('cols type error', (done) => {
+      assert.throws(() => {
         helper.modify(Model, 'user', 'string');
-      }, function(err) {
-        return err instanceof Error && err.message === "Allow modify attrs's name array"
-      });
+      }, (err) => err instanceof Error && err.message === "Allow modify attrs's name array");
       done();
     });
 
-    it("cols item type error", function(done) {
-      assert.throws(function() {
+    it('cols item type error', (done) => {
+      assert.throws(() => {
         helper.modify(Model, 'user', [null]);
-      }, function(err) {
-        return err instanceof Error && err.message === 'Every item in cols must be a string.';
-      });
+      }, (err) => err instanceof Error && err.message === 'Every item in cols must be a string.');
       done();
     });
 
-    it("cols item non-exists error", function(done) {
-      assert.throws(function() {
+    it('cols item non-exists error', (done) => {
+      assert.throws(() => {
         helper.modify(Model, 'user', ['id', 'price']);
-      }, function(err) {
-        return err instanceof Error && err.message === 'Attr non-exists: price';
-      });
+      }, (err) => err instanceof Error && err.message === 'Attr non-exists: price');
       done();
     });
-
   });
 
-  describe("All aguments validate passed", function() {
+  describe('All aguments validate passed', () => {
+    it('normal changed', (done) => {
+      const modify = helper.modify(Model, 'user', ['name']);
 
-    it("normal changed", function(done) {
-
-      var modify = helper.modify(Model, 'user', ['name']);
-
-      var req = {
+      const req = {
         hooks: {
           user: {
             id: 1,
             name: 'Redstone Zhao',
             age: 36,
-            changed: function() {
+            changed() {
               return ['name'];
             },
-            save: function(option) {
+            save(option) {
               assert.deepEqual({
-                fields: ['name']
+                fields: ['name'],
               }, option);
-              return new Promise(function(resolve, reject) {
-                setTimeout(function() {
+              return new Promise((resolve) => {
+                setTimeout(() => {
                   resolve({
                     id: 1,
                     name: 'Redstone Zhao',
-                    age: 36
+                    age: 36,
                   });
                 }, 20);
               });
-            }
-          }
+            },
+          },
         },
         params: {
           id: 99,
-          name: 'Redstone Zhao'
-        }
+          name: 'Redstone Zhao',
+        },
       };
 
-      var res = {
-        send: function(data) {
+      const res = {
+        send(data) {
           assert.deepEqual({
             id: 1,
             name: 'Redstone Zhao',
-            age: 36
+            age: 36,
           }, data);
-        }
+        },
       };
 
-      modify(req, res, function(error) {
+      modify(req, res, (error) => {
         assert.equal(null, error);
 
         done();
       });
-
     });
 
-    it("normal unchanged", function(done) {
+    it('normal unchanged', (done) => {
+      const modify = helper.modify(Model, 'user');
 
-      var modify = helper.modify(Model, 'user');
-
-      var req = {
+      const req = {
         hooks: {
           user: {
             id: 1,
             name: 'Redstone Zhao',
             age: 36,
-            changed: function() {
+            changed() {
               return false;
-            }
-          }
+            },
+          },
         },
         params: {
           id: 99,
-          name: 'Redstone Zhao'
-        }
+          name: 'Redstone Zhao',
+        },
       };
 
-      var res = {
-        send: function(data) {
+      const res = {
+        send(data) {
           assert.equal(req.hooks.user, data);
           assert.equal(true, req._resourceNotChanged);
         },
-        header: function(key, value) {
+        header(key, value) {
           assert.equal('X-Content-Resource-Status', key);
           assert.equal('Unchanged', value);
-        }
+        },
       };
 
-      modify(req, res, function(error) {
+      modify(req, res, (error) => {
         assert.equal(null, error);
 
         done();
       });
-
     });
 
-    it("Has error when save", function(done) {
+    it('Has error when save', (done) => {
+      const modify = helper.modify(Model, 'user');
 
-      var modify = helper.modify(Model, 'user');
-
-      var req = {
+      const req = {
         hooks: {
           user: {
             id: 1,
             name: 'Redstone Zhao',
             age: 36,
-            changed: function() {
+            changed() {
               return ['name'];
             },
-            save: function(option) {
+            save(option) {
               assert.deepEqual({
-                fields: ['name']
+                fields: ['name'],
               }, option);
-              return new Promise(function(resolve, reject) {
-                setTimeout(function() {
+              return new Promise((resolve, reject) => {
+                setTimeout(() => {
                   reject(Error('Hello world'));
                 }, 20);
               });
-            }
-          }
+            },
+          },
         },
         params: {
           id: 99,
-          name: 'Redstone Zhao'
-        }
+          name: 'Redstone Zhao',
+        },
       };
 
-      var res = {
-        send: function(data) {
+      const res = {
+        send(data) {
           assert.equal(req.hooks.user, data);
           assert.equal(true, req._resourceNotChanged);
         },
-        header: function(key, value) {
+        header(key, value) {
           assert.equal('X-Content-Resource-Status', key);
           assert.equal('Unchanged', value);
-        }
+        },
       };
 
-      modify(req, res, function(error) {
+      modify(req, res, (error) => {
         assert.ok(error instanceof Error);
         assert.equal('Hello world', error.message);
 
         done();
       });
-
     });
 
-    it("Has error when beforeModify", function(done) {
+    it('Has error when beforeModify', (done) => {
+      const modify = helper.modify(Model, 'user', ['name', 'age']);
 
-      var modify = helper.modify(Model, 'user', ['name', 'age']);
-
-      var req = {
+      const req = {
         hooks: {
           user: {
             id: 1,
             name: 'Redstone Zhao',
             age: 36,
-            changed: function() {
+            changed() {
               return ['name'];
             },
-            save: function() {
-              return new Promise(function(resolve, reject) {
-                setTimeout(function() {
+            save() {
+              return new Promise((resolve, reject) => {
+                setTimeout(() => {
                   reject(new Error('Has error when save'));
                 }, 10);
               });
             },
-          }
-        }
+          },
+        },
       };
 
-      var res = {
-        send: function(data) {
+      const res = {
+        send(data) {
           assert.equal(req.hooks.user, data);
           assert.equal(true, req._resourceNotChanged);
         },
-        header: function(key, value) {
+        header(key, value) {
           assert.equal('X-Content-Resource-Status', key);
           assert.equal('Unchanged', value);
-        }
+        },
       };
 
-      modify(req, res, function(error) {
+      modify(req, res, (error) => {
         assert.ok(error instanceof Error);
         assert.equal('Has error when save', error.message);
 
         done();
       });
     });
-
   });
-
 });
